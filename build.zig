da90cbfab04aa0bce82d38ab57fd6db6371df643
Ian McFarlane <i.mcfarlane2002@gmail.com>
2025-02-24 17:13:54 -0500

    added rlimgui

--- build.zig
+++ build.zig
@@ -13,6 +13,13 @@ const universal_flags = &[_][]const u8{
     "-DFMT_HEADER_ONLY",
     "-Werror",
     "-Wno-deprecated",
+    "-DIMGUI_DISABLE_OBSOLETE_FUNCTIONS",
+    "-DIMGUI_DISABLE_OBSOLETE_KEYIO",
+
+    // include vendor rlImGui
+    "-I./vendor/rlImGui/",
+    // for rlimgui
+    "-DNO_FONT_AWESOME",
 };
 
 const cpp_sources = &[_][]const u8{
@@ -42,6 +49,7 @@ pub fn build(b: *std.Build) !void {
     const raylib = b.dependency("raylib", .{ .target = target, .optimize = optimize, .linux_display_backend = .X11 });
     const chipmunk = b.dependency("chipmunk2d", .{ .target = target, .optimize = optimize, .use_doubles = false });
     const fastnoiselite = b.dependency("fastnoiselite", .{ .target = target, .optimize = optimize });
+    const imgui = b.dependency("imgui", .{ .target = target, .optimize = optimize, .backend = .SDL2 });
 
     // TODO: okaylib should propagate this to us but its not working, manually including it
     const fmt = b.dependency("fmt", .{});
@@ -50,6 +58,9 @@ pub fn build(b: *std.Build) !void {
 
     // add include flag for compile_commands.json
     try flags.append(b.fmt("-I{s}", .{b.pathJoin(&.{ fastnoiselite.builder.install_path, "include" })})); // just for compile commands to work properly?
+
+    // include imgui headers
+    try flags.append(b.fmt("-I{s}", .{b.pathJoin(&.{ imgui.builder.install_path, "include" })}));
 
     const final_flags = try flags.toOwnedSlice();
 
@@ -65,13 +76,43 @@ pub fn build(b: *std.Build) !void {
         .flags = final_flags,
     });
 
+    const rlimgui = b.addStaticLibrary(.{
+        .target = target,
+        .optimize = optimize,
+        .name = "rlimgui",
+    });
+
+    // build imgui ourselves, with rlgui, effectively ignoring the build.zig in imgui repo
+    rlimgui.addCSourceFiles(.{
+        .root = imgui.builder.path("."),
+        .files = &.{
+            "imgui.cpp",
+            "imgui_demo.cpp",
+            "imgui_draw.cpp",
+            "imgui_tables.cpp",
+            "imgui_widgets.cpp",
+        },
+        .flags = final_flags,
+    });
+    // fuse in imgui
+    rlimgui.addCSourceFiles(.{
+        .files = &.{"vendor/rlImGui/rlImGui.cpp"},
+        .flags = final_flags,
+    });
+    // need to build the source files before we can use them in this lib
+    rlimgui.step.dependOn(imgui.builder.getInstallStep());
+    rlimgui.linkLibCpp(); // imgui only needs libc, but rlimgui needs cpp <map>
+    // link raylib into this and then it will be propagated to exe
+    rlimgui.linkLibrary(raylib.artifact("raylib"));
+
+    exe.linkLibrary(rlimgui);
+
     // link libraries
     exe.step.dependOn(okaylib.builder.getInstallStep());
     exe.step.dependOn(fmt.builder.getInstallStep());
     exe.step.dependOn(fastnoiselite.builder.getInstallStep()); // just for compile commands working properly?
     exe.addIncludePath(okaylib.builder.path("include/"));
     exe.linkLibCpp();
-    exe.linkLibrary(raylib.artifact("raylib"));
     exe.linkLibrary(fastnoiselite.artifact("FastNoiseLite"));
     exe.linkLibrary(chipmunk.artifact("chipmunk"));
     // raylib's include paths are wrong, they include the headers directly with -I/path/to/include/header.h instead of -I/path/to/include
---
+++ vendor/rlImGui/imconfig.h
@@ -0,0 +1,3 @@
+#pragma once
+// shim to get build to work, dont actually add configurations for imgui here
+// as they may not be included everywhere properly
