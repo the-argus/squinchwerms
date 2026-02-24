ef403cecea332320ec92fc185277c38e1c87de0c
Ian McFarlane <i.mcfarlane2002@gmail.com>
2026-02-23 21:54:02 -0500

    cmakelists now pulls in package-managed prefixes

--- vendor/package_manager.sh
+++ vendor/package_manager.sh
@@ -17,16 +17,19 @@ SDL_INSTALL_DIR=$INSTALLS_DIR/sdl
 GLAZE_INSTALL_DIR=$INSTALLS_DIR/glaze
 BOX2D_INSTALL_DIR=$INSTALLS_DIR/box2d
 IMGUI_INSTALL_DIR=$INSTALLS_DIR/imgui
+FMT_INSTALL_DIR=$INSTALLS_DIR/fmt
 
 SDL_SRC_DIR=$SCRIPT_DIR/sdl
 GLAZE_SRC_DIR=$SCRIPT_DIR/glaze
 BOX2D_SRC_DIR=$SCRIPT_DIR/box2d
 IMGUI_SRC_DIR=$SCRIPT_DIR/imgui
+FMT_SRC_DIR=$SCRIPT_DIR/fmt
 
 SDL_BUILD_DIR=$BUILDS_DIR/sdl
 GLAZE_BUILD_DIR=$BUILDS_DIR/glaze
 BOX2D_BUILD_DIR=$BUILDS_DIR/box2d
 IMGUI_BUILD_DIR=$BUILDS_DIR/imgui
+FMT_BUILD_DIR=$BUILDS_DIR/fmt
 
 if [ ! -f $SDL_SRC_DIR/CMakeLists.txt ]; then
 	echo "missing SDL, make sure to run git submodule update --init --recursive"
@@ -42,6 +45,8 @@ if command -v mold &> /dev/null; then
 	LINKER_TYPE="MOLD"
 fi
 
+BUILD_TYPE="Debug"
+
 # TODO: probably be aggressive here and enable features so that errors happen if
 # you do not have all dependencies installed. requires checking for current
 # platform. or grep for things in CMakeCache.txt after configure step
@@ -49,7 +54,7 @@ fi
 # configure SDL
 cmake -S $SDL_SRC_DIR -B $SDL_BUILD_DIR \
 	-DCMAKE_LINKER_TYPE=$LINKER_TYPE \
-	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 	-DSDL_ASAN=OFF \
 	-DSDL_ASSERTIONS=paranoid \
@@ -75,7 +80,7 @@ cmake --install $SDL_BUILD_DIR --prefix $SDL_INSTALL_DIR
 # configure glaze
 cmake -S $GLAZE_SRC_DIR -B $GLAZE_BUILD_DIR \
 	-DCMAKE_LINKER_TYPE=$LINKER_TYPE \
-	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 	-Dglaze_DEVELOPER_MODE=OFF \
 	-Dglaze_DISABLE_ALWAYS_INLINE=ON \
@@ -88,7 +93,7 @@ cmake --install $GLAZE_BUILD_DIR --prefix $GLAZE_INSTALL_DIR
 
 # configure box2d
 cmake -S $BOX2D_SRC_DIR -B $BOX2D_BUILD_DIR \
-	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 	-DCMAKE_LINKER_TYPE=$LINKER_TYPE \
 	-DBOX2D_SAMPLES=OFF \
@@ -104,7 +109,7 @@ cmake --install $BOX2D_BUILD_DIR --prefix $BOX2D_INSTALL_DIR
 
 #configure imgui
 cmake -S $IMGUI_SRC_DIR -B $IMGUI_BUILD_DIR \
-	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
 	-DCMAKE_LINKER_TYPE=$LINKER_TYPE \
 	-DCMAKE_PREFIX_PATH=$SDL_INSTALL_DIR \
@@ -117,3 +122,27 @@ cmake --build $IMGUI_BUILD_DIR --parallel
 # install imgui
 cmake --install $IMGUI_BUILD_DIR --prefix $IMGUI_INSTALL_DIR
 
+# configure fmt
+cmake -S $FMT_SRC_DIR -B $FMT_BUILD_DIR \
+	-DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+	-DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
+	-DCMAKE_LINKER_TYPE=$LINKER_TYPE \
+	-DFMT_DOC=OFF \
+	-DFMT_TEST=OFF \
+	-DFMT_FUZZ=OFF \
+	-DFMT_CUDA_TEST=OFF \
+	-DFMT_UNICODE=ON
+
+# build fmt
+cmake --build $FMT_BUILD_DIR --parallel
+# install fmt
+cmake --install $FMT_BUILD_DIR --prefix $FMT_INSTALL_DIR
+
+OUTSTRINGS="$OUTPUT_ROOT_DIR/cmake_prefix_path"
+
+rm $OUTSTRINGS
+echo "$SDL_INSTALL_DIR" >> $OUTSTRINGS
+echo "$GLAZE_INSTALL_DIR" >> $OUTSTRINGS
+echo "$BOX2D_INSTALL_DIR" >> $OUTSTRINGS
+echo "$IMGUI_INSTALL_DIR" >> $OUTSTRINGS
+echo "$FMT_INSTALL_DIR" >> $OUTSTRINGS
